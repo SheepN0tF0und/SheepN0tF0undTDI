0a43d5597082875999722ed8062259b4305860d0
SheepN0tF0und <118444969+SheepN0tF0und@users.noreply.github.com>
2023-02-23 17:30:58 -0300

    Update ferramentas.sh
    -Corrigido formatação de cores saindo no output das instalações
    -instalação opcional do nmap e do openssh

--- ferramentas.sh
+++ ferramentas.sh
@@ -1,22 +1,22 @@
 #!/bin/bash
-#Script criado por SheepN0tF0und baseado no script da Lyanna  na intenção de auxiliar os alunos do TDI.
+#Script criado por SheepN0tF0und baseado no script da Lyanna na intenção de auxiliar os alunos do TDI.
 
 # Checa se o sistema é Linux ou não
 # Se for, o processo continua. Caso contrário, a aplicação será fechada.
 if [ "$(uname)" != "Linux" ]; then
-   echo -e "\e[1;32mVocê não está usando um sistema GNU/Linux ou similar"
+   echo -e "\e[1;32mVocê não está usando um sistema GNU/Linux ou similar\e[0m"
    exit 1
 fi
 
 clear
-echo -e "\e[1;36m Vamos começar atualizando o \e[1;32mTermux."
+echo -e "\e[1;36m Vamos começar atualizando o \e[1;32mTermux.\e[0m"
 
 sleep 4
 cd ~
 apt update -y && apt full-upgrade -y
 clear
 
-echo -e "\e[1;36m Instalando o Git e as duas linguagens necessarias \e[1;32mPython, Golang"
+echo -e "\e[1;36m Instalando o Git e as duas linguagens necessarias \e[1;32mPython, Golang\e[0m"
 
 sleep 5
 
@@ -31,7 +31,7 @@ apt install golang -y
 
 clear
 
-echo -e "\e[1;36m Instalando ferramentas em \e[1;32mGolang"
+echo -e "\e[1;36m Instalando ferramentas em \e[1;32mGolang\e[0m"
 
 sleep 5
 
@@ -52,7 +52,7 @@ go install -v github.com/projectdiscovery/nuclei/v2/cmd/nuclei@latest
 ~/go/bin/nuclei -update-templates
 
 clear
-echo -e "\e[1;36m Baixando repositórios de ferramentas de \e[1;32mPYTHON"
+echo -e "\e[1;36m Baixando repositórios de ferramentas de \e[1;32mPYTHON\e[0m"
 
 sleep 4
 # Baixando ferramentas do Github
@@ -76,8 +76,38 @@ pip install -r requirements.txt
 
 cd ~
 
-echo -e "\e[1;36mBaixando o \e[1;92mSeclist"
+echo -e "\e[1;36mBaixando o \e[1;92mSeclist\e[0m"
 
 sleep 5
 
 git clone https://github.com/danielmiessler/SecLists.git
+
+# Função de menu opcional para instalar o nmap e openssh
+menu() {
+    echo -e "Deseja instalar o \e[92mnmap\e[0m e o \e[34mopenssh?\e[0m"
+    echo -e "1 - Sim"
+    echo -e "2 - Não"
+    read opcao
+
+    case $opcao in
+        1)
+            echo -e "Instalando o nmap e o openssh"
+            apt install nmap -y
+            apt install openssh -y
+            ;;
+        2)
+            echo -e "Continuando sem instalar o nmap e openssh"
+            ;;
+        *)
+            echo -e "Opção inválida. Saindo."
+            exit 1
+    esac
+}
+
+menu
+
+clear
+
+echo -e "\e[1;92mInstalação concluida"
+
+exit 0
